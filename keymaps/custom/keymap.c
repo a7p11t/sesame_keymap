6f060978fcde2e1135a83f3656d176dfd2261077
junk <66475075+a7p11t@users.noreply.github.com>
2022-06-02 22:58:20 +0900

    style: Replace KC_TRNS with readable alias

--- keymaps/custom/keymap.c
+++ keymaps/custom/keymap.c
@@ -15,6 +15,9 @@
  */
 #include QMK_KEYBOARD_H
 
+// Define keycode alias
+#define ________ KC_TRNS
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
     [0] = LAYOUT_alice(
@@ -25,10 +28,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_LCTL,          KC_LALT,          KC_SPC,           KC_LGUI,  KC_SPC,           KC_RALT,                   KC_RCTL
     ),
     [1] = LAYOUT_alice(
-        KC_TRNS, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        KC_TRNS, KC_TRNS, KC_HOME, KC_UP,   KC_END,  KC_PGUP, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-        KC_TRNS, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS,
-                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, KC_TRNS, KC_TRNS,
-                 KC_TRNS,          KC_TRNS,          KC_TRNS,          KC_TRNS, KC_TRNS,          KC_TRNS,                   KC_TRNS
+        ________, KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
+        ________, ________, KC_HOME, KC_UP,   KC_END,  KC_PGUP, ________, ________, ________, ________, ________, ________, ________, ________, ________,
+        ________, KC_CAPS, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, ________, ________, ________, ________, ________, ________, ________,          ________,
+                 ________, ________, ________, ________, ________, ________, KC_VOLD, KC_VOLU, KC_MUTE, KC_MPRV, KC_MNXT, KC_MPLY, ________, ________,
+                 ________,          ________,          ________,          ________, ________,          ________,                   ________
     )
 };
