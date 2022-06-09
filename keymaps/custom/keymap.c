f2fb968e0fa3ba063822fdbdfaaa5a68f6787e10
junk <66475075+a7p11t@users.noreply.github.com>
2022-06-10 02:23:40 +0900

    feat: Add event handler for LOWER key

--- keymaps/custom/keymap.c
+++ keymaps/custom/keymap.c
@@ -42,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /**/ A_PSCR  , KC_GRV  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS ,
         /**/ KC_F5   , KC_TAB  , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,           KC_ENT  ,
         /**/           KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , A_IME   , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , MO_L    ,
-        /**/           KC_LCTL ,           KC_LALT , KC_LGUI ,           KC_SPC  , KC_ENT  ,                     MO_R    ,                     KC_RCTL
+        /**/           KC_LCTL ,           KC_LALT , KC_LGUI ,           LOWER   , KC_ENT  ,                     MO_R    ,                     KC_RCTL
     ),
     [KL_LOWER] = LAYOUT_alice(
         /**/ ________, KC_GRV  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_DEL  ,
@@ -66,3 +66,37 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /**/           ________,           ________, ________,           ________, ________,                     ________,                     ________
     )
 };
+
+static bool lower_pressed = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+        case LOWER:
+            if(record->event.pressed) {
+                lower_pressed = true;
+
+                layer_on(KL_LOWER);
+                update_tri_layer(KL_LOWER, KL_RAISE, KL_ADJUST);
+
+            } else {
+                layer_off(KL_LOWER);
+                update_tri_layer(KL_LOWER, KL_RAISE, KL_ADJUST);
+
+                if(lower_pressed) {
+                    register_code(KC_SPC);
+                    unregister_code(KC_SPC);
+                }
+                lower_pressed = false;
+            }
+            return false;
+            break;
+
+        default:
+            if(record->event.pressed) {
+                // reset the flag
+                lower_pressed = false;
+            }
+            break;
+    }
+    return true;
+}
