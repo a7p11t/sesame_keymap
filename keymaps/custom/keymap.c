56c321fcc5afbc30f0d3de17647793821358d5f8
junk <66475075+a7p11t@users.noreply.github.com>
2022-06-27 21:35:31 +0900

    feat: Define and assign alias IME key

--- keymaps/custom/keymap.c
+++ keymaps/custom/keymap.c
@@ -23,6 +23,7 @@
 
 // Define keycode alias
 #define ________ KC_TRNS
+#define A_IME LCTL(KC_SPC)
 #define MO_L MO(KL_LOWER)
 #define MO_R MO(KL_RAISE)
 
@@ -31,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /**/ KC_PAUS , KC_ESC  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , KC_BSPC ,
         /**/ KC_PSCR , KC_GRV  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS ,
         /**/ KC_F5   , KC_TAB  , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,           KC_ENT  ,
-        /**/           KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_B    , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , MO_L    ,
+        /**/           KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , A_IME   , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , MO_L    ,
         /**/           KC_LCTL ,           KC_LALT , KC_LGUI ,           KC_SPC  , KC_ENT  ,                     MO_R    ,                     KC_RCTL
     ),
     [KL_LOWER] = LAYOUT_alice(
         /**/ ________, KC_GRV  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_DEL  ,
         /**/ ________, ________, KC_HOME , ________, KC_END  , KC_PGUP , ________, ________, ________, ________, ________, ________, ________, ________, ________,
         /**/ ________, ________, ________, ________, ________, KC_PGDN , ________, KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , ________, ________,           ________,
-        /**/           ________, ________, ________, ________, ________, ________, KC_VOLD , KC_VOLU , KC_MUTE , KC_MPRV , KC_MNXT , KC_MPLY , ________, ________,
+        /**/           ________, ________, ________, ________, ________, A_IME   , KC_VOLD , KC_VOLU , KC_MUTE , KC_MPRV , KC_MNXT , KC_MPLY , ________, ________,
         /**/           ________,           ________, ________,           ________, ________,                     ________,                     ________
     ),
     [KL_RAISE] = LAYOUT_alice(
