cbc79687befbc3361e91d1c18cd98872016fa135
junk <66475075+a7p11t@users.noreply.github.com>
2022-07-09 12:16:02 +0900

    feat: Make IME key work in both ANSI and JIS

--- sesame/keymaps/custom/keymap.c
+++ sesame/keymaps/custom/keymap.c
@@ -26,12 +26,12 @@ enum custom_keycodes {
     QWERTY = SAFE_RANGE,
     LOWER,
     RAISE,
-    ADJUST
+    ADJUST,
+    IME
 };
 
 // Define keycode alias
 #define ________ KC_TRNS
-#define A_IME LCTL(KC_SPC)
 #define A_LOCK LGUI(KC_L)
 #define A_PSCR LGUI(KC_PSCR)
 #define MO_L MO(KL_LOWER)
@@ -41,14 +41,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         /**/ KC_PAUS , KC_ESC  , KC_1    , KC_2    , KC_3    , KC_4    , KC_5    , KC_6    , KC_7    , KC_8    , KC_9    , KC_0    , KC_MINS , KC_EQL  , KC_BSPC ,
         /**/ A_PSCR  , KC_GRV  , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_LBRC , KC_RBRC , KC_BSLS ,
         /**/ KC_F5   , KC_TAB  , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_QUOT ,           KC_ENT  ,
-        /**/           KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , A_IME   , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , MO_L    ,
+        /**/           KC_LSFT , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , IME     , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT , MO_L    ,
         /**/           KC_LCTL ,           KC_LALT , KC_LGUI ,           LOWER   , RAISE   ,                     KC_RALT ,                     KC_RCTL
     ),
     [KL_LOWER] = LAYOUT_alice(
         /**/ ________, KC_GRV  , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   , KC_F6   , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_F11  , KC_F12  , KC_DEL  ,
         /**/ ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________, ________,
         /**/ A_LOCK  , ________, KC_HOME , KC_PGDN , KC_PGUP , KC_END  , ________, KC_LEFT , KC_DOWN , KC_UP   , KC_RGHT , ________, ________,           ________,
-        /**/           ________, ________, ________, ________, ________, A_IME   , KC_VOLD , KC_VOLU , KC_MUTE , KC_MPRV , KC_MNXT , KC_MPLY , ________, ________,
+        /**/           ________, ________, ________, ________, ________, IME     , KC_VOLD , KC_VOLU , KC_MUTE , KC_MPRV , KC_MNXT , KC_MPLY , ________, ________,
         /**/           ________,           ________, ________,           ________, ________,                     ________,                     ________
     ),
     [KL_RAISE] = LAYOUT_alice(
@@ -110,6 +110,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 raise_pressed = false;
             }
             return false;
+            break;
+
+        case IME:
+            if(record->event.pressed) {
+                SEND_STRING(SS_LALT("`"));
+                // reset the flag
+                lower_pressed = false;
+                raise_pressed = false;
+            }
             break;
 
         default:
